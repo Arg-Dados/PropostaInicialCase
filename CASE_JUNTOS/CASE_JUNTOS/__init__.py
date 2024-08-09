073460b87657c59954bbd02c14e932a285c546a2
Arg-Dados <153097231+Arg-Dados@users.noreply.github.com>
2024-08-09 15:48:50 -0300

    Update __init__.py
    Adicionar loop while true para verificação contínua

--- CASE_JUNTOS/CASE_JUNTOS/__init__.py
+++ CASE_JUNTOS/CASE_JUNTOS/__init__.py
@@ -3,6 +3,7 @@ from utils_class.directoryManager import DirectoryManager
 from utils_class.dataLakeWriter import DataLakeWriter
 from utils_class.dataProcessor import DataProcessor
 from utils.data_schemas import schema
+from time import sleep
 import pandas as pd
 import sys
 import os
@@ -27,28 +28,37 @@ os.system('cls' if os.name == 'nt' else 'clear')
 print("Sessão Spark iniciada")
 
 # Lendo todos os arquivos encontrados na pasta de input inicial
-csv_files = os.listdir(directory_manager.get_storage_raw())
-
-for file in csv_files:
-    file_path = os.path.join(directory_manager.get_storage_raw(), file)
+while True:
     
-    # Verificar se é um arquivo CSV
-    if file.endswith('.csv'):
-        df = pd.read_csv(file_path)
+    while True:
+        csv_files = os.listdir(directory_manager.get_storage_raw())
+        if len(csv_files) > 0:
+            break
+        sleep(5)
+    
+    for file in csv_files:
+        file_path = os.path.join(directory_manager.get_storage_raw(), file)
         
-        # Processamento dos dados
-        data_processor = DataProcessor()
-        streaming_df = data_processor.save_as_stream(
-            df=df, 
-            schema=schema,
-            storage_temporary=directory_manager.get_storage_temporary()
+        # Verificar se é um arquivo CSV
+        if file.endswith('.csv'):
+            df = pd.read_csv(file_path)
+            
+            # Processamento dos dados
+            data_processor = DataProcessor()
+            streaming_df = data_processor.save_as_stream(
+                df=df, 
+                schema=schema,
+                storage_temporary=directory_manager.get_storage_temporary()
+                )
+            
+            # Escrita dos dados no Data Lake
+            data_lake_writer = DataLakeWriter(streaming_df=streaming_df)
+            data_lake_writer.write_data_lake(
+                storage_checkpoint=directory_manager.get_storage_checkpoint(),
+                storage_trusted=directory_manager.get_storage_trusted()
             )
-        
-        # Escrita dos dados no Data Lake
-        data_lake_writer = DataLakeWriter(streaming_df=streaming_df)
-        data_lake_writer.write_data_lake(
-            storage_checkpoint=directory_manager.get_storage_checkpoint(),
-            storage_trusted=directory_manager.get_storage_trusted()
-        )
-        
-        print(f"Streaming query finalizado para o arquivo: {file}")
+            
+            print(f"Streaming query finalizado para o arquivo: {file}")
+            
+        os.remove(file_path)
+        csv_files.pop(0)
